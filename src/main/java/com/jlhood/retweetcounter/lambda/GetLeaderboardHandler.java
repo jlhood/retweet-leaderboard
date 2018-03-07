c724a5170f61f347c89b954b551a7968faa2ff55
💻 💪 😉 James Hood <james@jlhood.com>
2018-03-07 15:03:50 -0800

    Add CORS header in response

--- src/main/java/com/jlhood/retweetcounter/lambda/GetLeaderboardHandler.java
+++ src/main/java/com/jlhood/retweetcounter/lambda/GetLeaderboardHandler.java
@@ -1,10 +1,13 @@
 package com.jlhood.retweetcounter.lambda;
 
+import java.util.Map;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.jlhood.retweetcounter.Leaderboard;
@@ -13,6 +16,7 @@ import com.jlhood.retweetcounter.dagger.DaggerAppComponent;
 
 public class GetLeaderboardHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
     private static final Gson GSON = new GsonBuilder().disableHtmlEscaping().create();
+    private static final Map<String, String> CORS_HEADERS = ImmutableMap.of("Access-Control-Allow-Origin", "*");
 
     private final Leaderboard leaderboard;
 
@@ -25,6 +29,7 @@ public class GetLeaderboardHandler implements RequestHandler<APIGatewayProxyRequ
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
         return new APIGatewayProxyResponseEvent()
                 .withStatusCode(200)
+                .withHeaders(CORS_HEADERS)
                 .withBody(GSON.toJson(leaderboard.load()));
     }
 }
